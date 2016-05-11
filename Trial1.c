847c788fc6f4c2dca299795f0e1e19094e5c202c
DanielleMelis <danielle.melis.cj@gmail.com>
2016-05-11 14:32:37 +0200

    initial commit

---
+++ Trial1.c
@@ -0,0 +1,41 @@
+// read graph from stream
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct Node{
+	int id;
+	struct List* edge;
+};
+
+struct List{
+	struct List* next;
+	struct Node* node;
+};
+
+
+
+int main(int argc, char* argv[]){
+
+	if (argc != 2){
+		printf("Invalid input");
+		exit(1);
+	}
+
+	FILE *file = fopen(argv[1], "r");
+	char *line = NULL;
+	size_t n = 0;
+
+	while (getline(&line, &n, file) > 0){
+		printf("%s", line);
+		
+		free(line);
+		line = NULL;
+	}
+
+	// may free line again
+
+	printf("done\n");
+
+}
