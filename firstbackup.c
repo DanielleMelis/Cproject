6f47ba84f3768aa2e20a1f23831ae1a13d24feac
DanielleMelis <danielle.melis.cj@gmail.com>
2016-06-20 16:50:59 +0200

    working

--- firstbackup.c
+++ firstbackup.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <time.h>
 
+int noOfVertices;
+int noOfEdges;
+
 struct Node{
 	int degree;
 	struct EdgeEnd* edge;
@@ -15,75 +18,42 @@ struct EdgeEnd{
 	int targetNodeIdx;
 };
 
-
-int main(int argc, char* argv[]){
-
-	if (argc != 2){
-		printf("Invalid input");
-		exit(1);
-	}
-
-	FILE *file = fopen(argv[1], "r");
-	char *line = NULL;
-	size_t n = 0;
-
-
+void getNodes(FILE *file, char *line, size_t n){
 	getline(&line, &n, file);
-	int noOfVertices = atoi(line);
-	printf("%d\n", noOfVertices);
+	noOfVertices = atoi(line);
+	//printf("DEBUG:	%d\n", noOfVertices);
 	free(line);
 	line=NULL;
+}
 
-	struct Node nodes[noOfVertices];
-	memset(nodes, 0, noOfVertices*sizeof(struct Node));
+void getEdges(struct Node *nodes, int edgeNode1, int edgeNode2){
+	//printf("DEBUG:	create node from %d to %d\n", edgeNode1, edgeNode2);
 
-	int noOfEdges = 0;
+	struct EdgeEnd *edgeNode1EdgeEnd = calloc(1, sizeof(struct EdgeEnd));
+	struct EdgeEnd *edgeNode2EdgeEnd = calloc(1, sizeof(struct EdgeEnd));
 
-	while(getline(&line, &n, file) > 0){
-		noOfEdges++;
+	edgeNode2EdgeEnd->otherSide = edgeNode1EdgeEnd;
+	edgeNode1EdgeEnd->otherSide = edgeNode2EdgeEnd;
 
-		char *left = strsep(&line, " ");
-		int edgeNode1 = atoi(left);
-		int edgeNode2 = atoi(line);
+	edgeNode1EdgeEnd->targetNodeIdx = edgeNode2;
+	edgeNode2EdgeEnd->targetNodeIdx = edgeNode1;
 
-	//	if(edgeNode1 == edgeNode2){			// ignore loops
-	//		continue;
-	//	}
+	edgeNode1EdgeEnd->next = nodes[edgeNode1].edge;	//edges with connections
+	nodes[edgeNode1].edge = edgeNode1EdgeEnd;
+	//printf("DEBUG:	add edge end to %d with target node idx %d\n", edgeNode1, edgeNode1EdgeEnd->targetNodeIdx);
 
-		printf("create node from %d to %d\n", edgeNode1, edgeNode2);
+	edgeNode2EdgeEnd->next = nodes[edgeNode2].edge;
+	nodes[edgeNode2].edge = edgeNode2EdgeEnd;
+	//printf("DEBUG:	add edge end to %d with target node idx %d\n", edgeNode2, edgeNode2EdgeEnd->targetNodeIdx);
 
-		struct EdgeEnd *edgeNode1EdgeEnd = calloc(1, sizeof(struct EdgeEnd));
-		struct EdgeEnd *edgeNode2EdgeEnd = calloc(1, sizeof(struct EdgeEnd));
+	nodes[edgeNode1].degree++;
+	nodes[edgeNode2].degree++;
+}
 
-		edgeNode2EdgeEnd->otherSide = edgeNode1EdgeEnd;
-		edgeNode1EdgeEnd->otherSide = edgeNode2EdgeEnd;
-
-		edgeNode1EdgeEnd->targetNodeIdx = edgeNode2;
-		edgeNode2EdgeEnd->targetNodeIdx = edgeNode1;
-
-		edgeNode1EdgeEnd->next = nodes[edgeNode1].edge;	//edges with connections
-		nodes[edgeNode1].edge = edgeNode1EdgeEnd;
-		printf("add edge end to %d with target node idx %d\n", edgeNode1, edgeNode1EdgeEnd->targetNodeIdx);
-
-		edgeNode2EdgeEnd->next = nodes[edgeNode2].edge;
-		nodes[edgeNode2].edge = edgeNode2EdgeEnd;
-		printf("add edge end to %d with target node idx %d\n", edgeNode2, edgeNode2EdgeEnd->targetNodeIdx);
-
-		nodes[edgeNode1].degree++;
-		nodes[edgeNode2].degree++;
-
-		free(left);
-		//free(line);
-		line = NULL;
-	}
-
-	// check for n euler path
-	// make it more elegant
-
+int eulerCheck(struct Node *nodes){
 	int no_of_odd_nodes = 0;
 	int temp_nodeindex = 0;
 	int keepMe[2];
-
 
 	while(temp_nodeindex < noOfVertices){
 		if(nodes[temp_nodeindex].degree % 2 == 1){
@@ -95,54 +65,67 @@ int main(int argc, char* argv[]){
 		temp_nodeindex++;		
 	}
 
-	printf("DEBUG number of vertices with odd degree: %d\n", no_of_odd_nodes );
-
-	int currentNode;
-
 	if(no_of_odd_nodes == 2){
-		currentNode = keepMe[0];
-		printf("START with %d \n", currentNode);
+		return keepMe[0];
+		//printf("START with %d \n", currentNode);
 	}else if(no_of_odd_nodes == 0){
 		//start anywhere
-		printf("You may start anywhere");
+		//printf("You may start anywhere");
 		srand(time(NULL));
-		currentNode = ( rand() % noOfVertices);
+		return ( rand() % noOfVertices);
 	}else if(no_of_odd_nodes != 2){
 		printf("Euler path is not possible\n");
-		
+		exit(-1);
 	}
+}
+
+int main(int argc, char* argv[]){
+
+	if (argc != 2){
+		printf("Invalid input");
+		exit(1);
+	};
+
+	FILE *file = fopen(argv[1], "r");
+	char *line = NULL;
+	size_t n = 0;
+
+	getNodes(file, line, n);
+
+	struct Node nodes[noOfVertices];
+	memset(nodes, 0, noOfVertices*sizeof(struct Node));
+	
+	while(getline(&line, &n, file) > 0){
+		char *left = strsep(&line, " ");
+		int edgeNode1 = atoi(left);
+		int edgeNode2 = atoi(line);
+		getEdges(nodes, edgeNode1, edgeNode2);
+		noOfEdges++;
+
+		free(left);	
+		line = NULL;
+
+	}
+	free(line);
+
+	int currentNode = eulerCheck(nodes);
 
 	int noOfPassedEdges = 0;
 	
 	int *allPossibilities = malloc(noOfVertices*sizeof(int));
 	int counter = 0;
 	int *backMeup = malloc(noOfEdges*sizeof(int));
-	backMeup[counter] = currentNode;
-	
+	backMeup[counter] = currentNode;	
 
-//	printf("%d  ", currentNode);
+	printf("%d\n", currentNode);
+
 LOOPMEBACK: 	
 
 while(noOfPassedEdges != noOfEdges){
-	noOfPassedEdges++;
-		
+	noOfPassedEdges++;	
 		
 		int target = -1;
-		struct EdgeEnd *currentConnection = nodes[currentNode].edge;
-
-
-		// for(int index = 0; index < noOfVertices; index++){
-		// 	struct EdgeEnd *currentTest = nodes[index].edge;
-
-		// 	printf(" DEBUG %d - %d visited tag: %d\n", index, nodes[index].edge -> targetNodeIdx, nodes[index].edge -> visited);
-			
-		// 	if( currentTest->next != NULL ){
-		// 		currentTest = nodes[index].edge->next;	
-		// 		printf(" DEBUG %d - %d visited tag: %d\n", index, currentTest->targetNodeIdx, currentTest->visited);
-		// 	}
-
-		// }		
-		
+		struct EdgeEnd *currentConnection = nodes[currentNode].edge;	
 
 		while(target == -1){		
 
@@ -225,9 +208,7 @@ while(noOfPassedEdges != noOfEdges){
 	// 	printf(" DEBUG %d - %d visited tag: %d\n", nodeMe, tryingmyBest->targetNodeIdx, tryingmyBest->visited);
 	// }
 
-
-	free(line);
-	free(backMeup);
+	//free(backMeup);
 	free(allPossibilities);
 	printf("done\n");
 }
